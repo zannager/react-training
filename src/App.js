f2d52f3c4c636d3d17f8ee4864344d2909cf25e3
zannager <gzraenga@gmail.com>
2024-06-15 01:43:13 +0800

    learning react

--- src/App.js
+++ src/App.js
@@ -1,33 +1,55 @@
-import React from 'react';
+// import React from 'react';
+import React, { Component } from 'react'; 
 import logo from './logo.svg';
 import './App.css';
 import { useState } from "react";
 import ReactDOM from "react-dom/client";
+import axios from 'axios';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-  class Car extends React.Component {
-    render() {
-      return <h2>Hi, I am a Car!</h2>;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Let's try something different!
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React - Fundamentals
+//         </a>
+//       </header>
+//     </div>
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    // state
+    this.state = {
+      users: []
     }
-  };
+  }
+
+  componentDidMount() {
+    axios.get('https://api.randomuser.me/?nat=US$result=5')
+    .then(response => {
+      console.log(response);
+      this.setState({users: response.data.results});
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
+  }
+
+  render() {
+    return <div className='App'>Learning is fun</div>
+  }
 }
 
 export default App;
