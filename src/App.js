1a0f245924050c3bd4c1c51a17393524f79f900d
zannager <gzraenga@gmail.com>
2024-06-20 01:54:31 +0800

    react hooks

--- src/App.js
+++ src/App.js
@@ -130,20 +130,29 @@ import { render } from '@testing-library/react';
 
 const App = () => {
   //state
-  const [news, setNews] = useState([])
+  const [news, setNews] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('react')
   // fetch news
   const fetchNews = () => {
-    fetch('http://hn.algolia.com/api/v1/search?query=react')
+    fetch(`http://hn.algolia.com/api/v1/search?query=react=${searchQuery}`)
     .then(result => result.json())
     .then(data => setNews(data.hits))
     .catch(error => console.log(error));
   }
   useEffect(() => {
-    fetchNews()
-  })
+    fetchNews();
+  }, [searchQuery]);
+  const handleChage = (e) => {
+    setSearchQuery(e.target.value)
+  }
+  
   return(
     <div>
       <h2>News</h2>
+      <form>
+        <input type='text' value={searchQuery} onChange={handleChage}/>
+        <button>Search</button>
+      </form>
       {news.map((n, i) => (<p key={i}>{n.title}</p>))}
     </div>
   )
