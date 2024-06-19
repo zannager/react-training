342d53ca10e9e29fdfce8313668954c00bccaa48
zannager <gzraenga@gmail.com>
2024-06-20 02:27:01 +0800

    react hooks

--- src/App.js
+++ src/App.js
@@ -132,12 +132,16 @@ const App = () => {
   //state
   const [news, setNews] = useState([]);
   const [searchQuery, setSearchQuery] = useState('react')
-  const [url, setUrl] = useState('http://hn.algolia.com/api/v1/search?query=react')
+  const [url, setUrl] = useState('http://hn.algolia.com/api/v1/search?query=react');
+  const [loading, setLoading] = useState(false);
+
   // fetch news
   const fetchNews = () => {
+    //set loading true
+    setLoading(true)
     fetch(url)
     .then(result => result.json())
-    .then(data => setNews(data.hits))
+    .then(data => (setNews(data.hits), setLoading(false)))
     .catch(error => console.log(error));
   }
   useEffect(() => {
@@ -151,15 +155,26 @@ const App = () => {
     e.preventDefault();
     setUrl(`http://hn.algolia.com/api/v1/search?query=react=${searchQuery}`);
   }
+
+  const showLoading = () => (loading ? <h2>Loading...</h2> : "")
+
+  const searchForm = () => (
+    <form onSubmit={handleSubmit}>
+        <input type='text' value={searchQuery} onChange={handleChage}/>
+        <button>Search</button>
+      </form>
+  )
+
+  const showNews = () => (
+    news.map((n, i) => (<p key={i}>{n.title}</p>))
+  )
   
   return(
     <div>
       <h2>News</h2>
-      <form onSubmit={handleSubmit}>
-        <input type='text' value={searchQuery} onChange={handleChage}/>
-        <button>Search</button>
-      </form>
-      {news.map((n, i) => (<p key={i}>{n.title}</p>))}
+      {showLoading()}
+      {searchForm()}
+      {showNews()}
     </div>
   )
 }
