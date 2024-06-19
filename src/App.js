fe588f19813bd4173e189184ac92edb73aa69076
zannager <gzraenga@gmail.com>
2024-06-19 11:45:20 +0800

    react hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // import React from 'react';
 import React, { Component } from 'react'; 
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 import { useState } from "react";
 import ReactDOM from "react-dom/client";
 import axios from 'axios';
 import { render } from '@testing-library/react';
-import Loading from './Loading'
+// import Loading from './Loading'
 
 
 // function App() {
@@ -28,57 +28,78 @@ import Loading from './Loading'
 //       </header>
 //     </div>
 
-class App extends Component{ 
-  constructor(props) {
-    super(props)
-    // state
-    this.state = {
-      users: [],
-      loading: false
-    };
-    //bind
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+// class App extends Component{ 
+//   constructor(props) {
+//     super(props)
+//     // state
+//     this.state = {
+//       users: [],
+//       loading: false
+//     };
+//     //bind
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
 
-  getUsers() {
-    this.setState({
-      loading: true
-    })
-    axios.get('https://api.randomuser.me').then(response =>  this.setState({
-      users: [...this.state.users, ...response.data.results],
-      loading: false
-      })
-    );
-  }
+//   getUsers() {
+//     this.setState({
+//       loading: true
+//     })
+//     axios.get('https://api.randomuser.me').then(response =>  this.setState({
+//       users: [...this.state.users, ...response.data.results],
+//       loading: false
+//       })
+//     );
+//   }
 
-  handleSubmit(e) {
-    e.preventDefault();
-   this. getUsers();
-   console.log('more users loaded');
-  }
+//   handleSubmit(e) {
+//     e.preventDefault();
+//    this. getUsers();
+//    console.log('more users loaded');
+//   }
 
-  componentDidMount() {
-    this.getUsers();
-  }
+//   componentDidMount() {
+//     this.getUsers();
+//   }
 
-  render() {
-    const {loading, users} = this.state
-    return <div className='App'>
-      <h1>Hello World</h1>
-      <form onSubmit={this.handleSubmit}>
-          <input type='submit' value='load users' />
-        </form>
-      {!loading ? users.map(user => 
-        <div key={user.login.uuid}>
-        <h1 style={{color: 'red'}}>{user.name.first}</h1>
-        <p>{user.email}</p>
+//   render() {
+//     const {loading, users} = this.state
+//     return <div className='App'>
+//       <h1>Hello World</h1>
+//       <form onSubmit={this.handleSubmit}>
+//           <input type='submit' value='load users' />
+//         </form>
+//       {!loading ? users.map(user => 
+//         <div key={user.login.uuid}>
+//         <h1 style={{color: 'red'}}>{user.name.first}</h1>
+//         <p>{user.email}</p>
       
-        </div>)
-        : <Loading message="nice nice" />}
+//         </div>)
+//         : <Loading message="nice nice" />}
     
+//       </div>
+//   }
+// }
+
+// export default App;
+
+class App extends Component {
+  state = {
+    count: 0
+  }
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+  render() {
+    return(
+      <div>
+        <h2>counter app</h2>
+        <button onClick={this.increment}>Clicked {this.state.count} times</button>
       </div>
+      
+    );
   }
 }
 
 export default App;
-
